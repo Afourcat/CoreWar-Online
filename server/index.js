aae716896a509081a5f51e2754824b3f4b910342
afourcat <alexandre.fourcat@epitech.eu>
2018-04-19 17:21:35 +0000

    Salam

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const io = require('socket.io')(server);
 const path = require('path');
 var bodyParser = require('body-parser');
 
-app.use('/combat-template', express.static('views/template/'));
 app.use(express.static(path.join(__dirname, '/views/static')));
 app.use(bodyParser.urlencoded({ extended: true })); 
 var lobby_name = "Default";
@@ -21,21 +20,36 @@ app.get('/combat/', function(req, res) {
 var lobby = io.of('/lobby');
 
 app.get('/lobby/', function(req, res) {
-	lobby.emit('lobby-name', lobby_name);
 	res.sendFile(__dirname + "/views/template/lobby.html")
 });
 
-app.post('/lobby/', function (req, res) {
-	lobby_name = req.body.name;
-	res.redirect('/lobby/');
+lobby.on('connection', function(socket) {
+	socket.emit('lobby-name', lobby_name);
 });
 
-server.listen(3000, function() {
-	console.log("Listenning on port 3000");
+lobby.on('input-file', function (data) {
+	console.log('SALUT', data);
+});
+
+app.post('/warrior', function(req, res) {
+	var warrior0 = req.body['warrior0'];
+	var warrior1 = req.body['warrior1'];
+	var warrior2 = req.body['warrior2'];
+	var warrior3 = req.body['warrior3'];
+	console.log("POPO" , warrior0);
+});
+
+app.post('/lobby/', function (req, res) {
+	lobby_name = req.body['suce'];
+	res.redirect('/lobby/');
 });
 
 var combat = io.of('/combat');
 
 combat.on('connection', function(socket) {
-	console.log('someone connected');
+	console.log('socket ' + socket.id + ' connected on /combat');
+});
+
+server.listen(3000, function() {
+	console.log("Listenning on port 3000");
 });
